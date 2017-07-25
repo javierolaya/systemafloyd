03a3e751ed36e48655e60627a36688d6ee9a2caf
Javier Olaya <jolaya@fau.edu>
2017-07-25 11:20:51 -0400

    Thumbnail

--- page other.php
+++ page other.php
@@ -19,7 +19,7 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
 <?php endif; ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
-                <div class="header" style="background-image:"><?php get_the_post_thumbnail_url() ?>
+                <div class="header" style="background-image:"><?php echo: get_the_post_thumbnail_url() ?>
                 </div>
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
