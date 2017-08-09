75729b38710e471fc9afbcbe1253f6716d5c74b8
Yami <yami.cardona@WGInc.com>
2017-08-09 16:43:32 -0400

    logo-test

--- footer.php
+++ footer.php
@@ -14,7 +14,7 @@ if ( ! is_page_template( 'page-template-blank.php' ) ) : ?>
       
                     <div id="footer-bottom">
                         <div class="container clearfix">
-                            <div class="footer-section logo">
+                            <div class="logo">
                               <img src="http://systemafloyd.com/wp-content/uploads/2017/08/Logo.png">
                             </div>
                             
